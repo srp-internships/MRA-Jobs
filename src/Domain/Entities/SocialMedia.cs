7181cb6cd6af10c6277219f9e3a0b330284b6fbb
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-05-04 09:24:12 +0500

    SocialMedia Created

---
+++ src/Domain/Entities/SocialMedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MRA.Jobs.Domain.Enums;
+
+namespace MRA.Jobs.Domain.Entities;
+public class SocialMedia:BaseEntity
+{
+    public string Portfolio { get; set; }
+    public SocialMediaType SocialMediaType { get; set; }
+}
---
+++ src/Domain/Enums/SocialMediaType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Jobs.Domain.Enums;
+public enum SocialMediaType
+{
+    LinkedIn =1,
+    GitHub =2,
+    Twitter =3,
+    Facebook =4,
+    Instagram =5
+}
